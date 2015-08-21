19a6a23294d33f6be0222a7abac9bfe6f516bc99
covito <covito@163.com>
2015-08-21 16:23:10 +0800

    hh

--- brent-console-service/src/main/java/cn/brent/console/StartService.java
+++ brent-console-service/src/main/java/cn/brent/console/StartService.java
@@ -2,39 +2,47 @@ package cn.brent.console;
 
 import org.zeromq.ZMQ;
 
+import cn.brent.bus.BusStarter;
 import cn.brent.bus.rpc.RpcWorkHandler;
 import cn.brent.bus.util.Prop;
 import cn.brent.bus.worker.WorkerContext;
 import cn.brent.console.serivce.UserServiceImpl;
 
 public class StartService {
-	
-	public static final String CONFIG="bus-service.properties";
+
+	public static final String CONFIG = "bus-service.properties";
 
 	public static void main(String[] args) {
-		
-		String config=CONFIG;
-		if(args.length>0){
-			config=args[0];
+		String config = null;
+		if (args.length > 0) {
+			config = args[0];
 		}
-		Prop pro=new Prop(config);
-		
-		String broker=pro.get("brokers","127.0.0.1:15555");
-		int ioThreads=pro.getInt("ioThreads",1);
-		String registerToken=pro.get("registerToken");
-		
-		String[] brokers=broker.split(",");
-		
-		WorkerContext wc=new WorkerContext(ZMQ.context(ioThreads),brokers,registerToken);
-		
-		register(wc);
-		
+		BusStarter.start(null);
+		start(config);
 	}
-	
-	protected static void register(WorkerContext wc){
-		RpcWorkHandler rpc=new RpcWorkHandler(UserService.NAME);
+
+	public static void start(String config) {
+		
+		if(config==null){
+			config=CONFIG;
+		}
+		Prop pro = new Prop(config);
+
+		String broker = pro.get("brokers", "127.0.0.1:15555");
+		int ioThreads = pro.getInt("ioThreads", 1);
+		String registerToken = pro.get("registerToken");
+
+		String[] brokers = broker.split(",");
+
+		WorkerContext wc = new WorkerContext(ZMQ.context(ioThreads), brokers, registerToken);
+
+		register(wc);
+	}
+
+	protected static void register(WorkerContext wc) {
+		RpcWorkHandler rpc = new RpcWorkHandler(UserService.NAME);
 		rpc.addVersion(new UserServiceImpl());
-		wc.registerWorker(rpc,1);
+		wc.registerWorker(rpc, 1);
 	}
 
 }
